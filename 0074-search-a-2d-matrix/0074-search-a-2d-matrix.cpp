dfaf299c4900a30cf232cb5ca5c2c48ae725db0a
mradul-08 <mradulgarg2005@gmail.com>
2026-03-25 18:40:11 +0530

    Time: 0 ms (100%), Space: 13.3 MB (44.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,20 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-             int low = 0;
-             int high = (n*m -1);
 
-             while(low <= high){
-            int mid = (low+ high)/2;
-                    int row = mid/m;
-                    int column = mid% m;
+        int row = 0, column = m-1;
 
-                    if(matrix[row][column] == target) return true;
-                    else if(matrix[row][column] < target){ low = mid + 1;}
-                    else{
-                        high = mid - 1;
-                    }
-             }
-             return false;
+        while(row <n && column >= 0){
+            if(matrix[row][column] == target) return true;
+            else if(matrix[row][column] < target){
+                row++;
+            }
+            else{
+
+                     column--;
+            }
+        } 
+        return false;
     }
 };
