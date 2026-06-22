69a3d8b0845f7027e9053ac009b34ab74c363cdb
mradul-08 <mradulgarg2005@gmail.com>
2026-06-23 01:54:38 +0530

    Time: 46 ms (12.28%), Space: 11.7 MB (86.45%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int m, n;
+    int t[501][501];
+
+    int solve(string& s1, string& s2, int i, int j) {
+
+        if (i == m)
+            return n - j;
+
+        if (j == n)
+            return m - i;
+
+        if (t[i][j] != -1)
+            return t[i][j];
+
+        if (s1[i] == s2[j]) {
+            return t[i][j] = solve(s1, s2, i + 1, j + 1);
+        }
+
+        int insertC  = 1 + solve(s1, s2, i, j + 1);
+        int deleteC  = 1 + solve(s1, s2, i + 1, j);
+        int replaceC = 1 + solve(s1, s2, i + 1, j + 1);
+
+        return t[i][j] = min({insertC, deleteC, replaceC});
+    }
+
+    int minDistance(string word1, string word2) {
+
+        m = word1.size();
+        n = word2.size();
+
+        memset(t, -1, sizeof(t));
+
+        return solve(word1, word2, 0, 0);
+    }
+};
